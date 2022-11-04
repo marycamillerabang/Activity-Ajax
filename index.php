40eea3afacf77b4dccd114556b4ed5a89cd378f8
Mary Camille Rabang <rabangmarycamille@gmail.com>
2022-11-04 12:32:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Activity</title>
+    <title>Activity Ajax</title>
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -90,7 +90,7 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" id="inputGroup-sizing-default">ID</span>
                                     </div>
-                                    <input type="number" name="id"class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    <input type="number" name="id"class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" readonly>
                                     </div>
 
                                     <div class="input-group mb-3">
